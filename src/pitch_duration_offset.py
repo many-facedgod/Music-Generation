60623db94e7d1aed37d4c4a3dc963ef87563ea13
Ramesh Balaji <rameshb@Rameshs-MacBook-Pro.local>
2018-11-29 19:20:30 -0500

    Added Exception handling for processing files

--- src/pitch_duration_offset.py
+++ src/pitch_duration_offset.py
@@ -1,9 +1,18 @@
 from utils.reading import *
+import numpy as np
+import time
 
-INPUT_FILE = "../bach/aof/*.mid"
+start=time.time()
+
+INPUT_FILE = "../lakh_midi/files_200/*.mid"
 encoded_files, _, _, _ = process(INPUT_FILE)
 print("encoded_files.shape = ", encoded_files.shape)
 for f in encoded_files:
 	print(f.shape)
+np.save('../data/music_lmd.npy',encoded_files)
+
+end=time.time()
+
+print('Time taken for processing:', end-start)
 
 # ADD TRAINING CODE HERE, LIKE IN BASELINE.PY
